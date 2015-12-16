ff47a2e184cae693a1756cb40f6d94d1a6e19624
Mehmet Goren <mehmetgoren@outlook.com>
2015-12-16 18:51:04 +0200

    1

--- ionix.Data/MetaData/Extensions.cs
+++ ionix.Data/MetaData/Extensions.cs
@@ -1,4 +1,7 @@
-﻿namespace ionix.Data
+﻿using ionix.Utils;
+using ionix.Utils.Reflection;
+
+namespace ionix.Data
 {
     using System;
     using System.ComponentModel.DataAnnotations.Schema;
@@ -22,11 +25,12 @@
             if (!String.IsNullOrEmpty(xml))
             {
                 StringReader sr = new StringReader(xml);
-                XmlSerializer xs = new XmlSerializer(cachedType ?? typeof(T));
+                XmlSerializer xs = new XmlSerializer(cachedType ?? typeof (T));
                 return xs.Deserialize(sr) as T;
             }
             return null;
         }
+
         public static string Serialize(IXmlSerializable xs)
         {
             if (null == xs)
@@ -34,6 +38,7 @@
 
             return Serialize(xs, null);
         }
+
         public static T Deserialize<T>(string xml)
             where T : class, IXmlSerializable
         {
@@ -52,11 +57,52 @@
             if (null != ta)
                 return String.IsNullOrEmpty(ta.Schema) ? ta.Name : ta.Schema + '.' + ta.Name;
 
-            return entityType.FullName;//schema, user name gibi yapıları namespace yap.
+            return entityType.FullName; //schema, user name gibi yapıları namespace yap.
         }
+
         public static string GetTableName<TEntity>()
         {
-            return GetTableName(typeof(TEntity));
+            return GetTableName(typeof (TEntity));
+        }
+    }
+
+    public static class EntityMetadaExtensions
+    {
+        //SchemaInfo' ya göre validation. Zaten Typesafect bir ortamdayız. Sadece isnullable ve maxlength validation' ı.
+        //Kullanıcı isteğe göre ekleyebilir.
+        public static bool IsEntityValid<TEntity>(TEntity entity, IEntityMetaDataProvider provider)
+        {
+            if (null != entity && null != provider)
+            {
+                foreach (var metaData in provider.CreateEntityMetaData(typeof (TEntity)).Properties)
+                {
+                    PropertyInfo pi = metaData.Property;
+                    if (!metaData.Schema.IsNullable)
+                    {
+                        if (pi.PropertyType.IsClass || pi.PropertyType.IsNullableType())
+                            //ValueType zaten default value zore lar
+                        {
+                            object value = pi.GetValue(entity);
+                            if (null == value)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    int maxLength = metaData.Schema.MaxLength;
+                    if (maxLength > 0)
+                    {
+                        if (pi.PropertyType == CachedTypes.String)
+                        {
+                            object value = pi.GetValue(entity);
+                            if (value?.ToString().Length > maxLength)
+                                return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
