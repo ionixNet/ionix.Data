78264c9fda2c4faed485244fba98ab97182bcada
Mehmet Goren <mehmetgoren@outlook.com>
2016-02-18 19:14:48 +0200

    ParamatreName Çakışması test için copy eklendi

---
+++ ionixTests/EntityMetaDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ionix.Data;
+using ionix.Utils;
+using ionix.Utils.Serialization;
+using ionixTests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ionixTests
+{
+    [TestClass]
+    public class EntityMetaDataProviderTest
+    {
+        [TestMethod]
+        public void CopyTest()
+        {
+            IEntityMetaDataProvider provider = new DbSchemaMetaDataProvider();
+
+            var metaData = provider.CreateEntityMetaData(typeof (Invoices));
+
+            int len = metaData.Properties.Count();
+
+            Stopwatch bench = Stopwatch.StartNew();
+            for (int j = 0; j < 1000000; ++j)
+                metaData.Copy();
+            bench.Stop();
+
+            Debug.WriteLine("Schema Xml Copy: " +  bench.ElapsedMilliseconds);
+
+            Assert.IsNotNull(metaData);
+        }
+    }
+}
